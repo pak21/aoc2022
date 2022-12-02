387faad1180478bebf5f8a47302f634388f22972
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-02 18:41:07 +0000

    Ooops, another typo.

--- 02/02b.py
+++ 02/02b.py
@@ -13,7 +13,7 @@ def scores(line):
     b = ord(b) - 88
     score = 1 + b + result(a, b)
     b2 = (a + b + 2) % 3
-    score2 = 1 + b2 + result(a, b)
+    score2 = 1 + b2 + result(a, b2)
     return score, score2
 
 with open(sys.argv[1]) as f:
