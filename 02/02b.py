b45667c895fc9ff3d07406cfa1f0827b29e7551d
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-02 18:36:00 +0000

    Direct maths version of Day 2.

---
+++ 02/02b.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+import numpy as np
+
+def result(a, b):
+    return ((1 + (b - a) % 3) * 3) % 9
+
+def scores(line):
+    a, b = line.split(' ')
+    a = ord(opp) - 65
+    b = ord(me) - 88
+    score = 1 + b + result(a, b)
+    b2 = (a + b + 2) % 3
+    score2 = 1 + b2 + result(a, b)
+    return score, score2
+
+with open(sys.argv[1]) as f:
+    print(np.sum([scores(l.strip()) for l in f], axis=0))
