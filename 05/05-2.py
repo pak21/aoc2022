98202a33791393ce8f965367d53f4808b7470009
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-05 09:53:10 +0000

    Bit tidier, not much though.

--- 05/05-2.py
+++ 05/05-2.py
@@ -7,21 +7,20 @@ with open(sys.argv[1]) as f:
     t = f.read().rstrip()
     start_t, moves_t = t.split('\n\n')
 
-    config = collections.defaultdict(list)
+    config = [[]] * 9
     for l in start_t.split('\n'):
-        for i in range(0, len(l), 4):
-            t = l[i:i+3]
+        for i in range((len(l) + 1) // 4):
+            t = l[4*i:4*i+3]
             if t[0] == '[':
-                config[1 + i//4] = [t[1]] + config[1 + i//4]
+                config[i] = [t[1]] + config[i]
 
-    moves = []
     for l in moves_t.split('\n'):
         _, n, _, src, _, dest = l.split()
-        moves.append((int(n), int(src), int(dest)))
+        n = int(n)
+        src = int(src) - 1
+        dest = int(dest) - 1
 
-    for n, src, dest in moves:
         config[dest] = config[dest] + config[src][-n:]
         config[src] = config[src][:-n]
 
-    p1 = ''.join([config[1+i][-1] for i in range(len(config))])
-    print(p1)
+    print(''.join([stack[-1] for stack in config]))
