139e02acb8fe7b230d1eb3efff13270990e9c456
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-06 05:20:37 +0000

    Remove a line.

--- 06/06.py
+++ 06/06.py
@@ -7,7 +7,6 @@ n = int(sys.argv[2])
 with open(sys.argv[1]) as f:
     for l in f:
         for i in range(len(l)):
-            x = set(l[i:i+n])
-            if len(x) == n:
+            if len(set(l[i:i+n])) == n:
                 print(i+n)
                 break
