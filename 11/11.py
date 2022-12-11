afc75773f3ae8b548a3e43377ca8a78771eca50c
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-11 06:07:27 +0000

    Tidy a bit.

--- 11/11.py
+++ 11/11.py
@@ -3,29 +3,29 @@
 import functools
 import sys
 
-import numpy as np
-
 def parse_monkey(m):
     _, items_text, operation_text, test_text, true_text, false_text = m.split('\n')
     items = [int(i.replace(',', '')) for i in items_text.lstrip().split(' ')[2:]]
-    operation = operation_text.lstrip().split(' ')[4:]
+    operator, value = operation_text.lstrip().split(' ')[4:]
+    operation_lambda = (lambda a, b: a * b) if operator == '*' else (lambda a, b: a + b)
+    match value:
+        case 'old':
+            value_lambda = lambda x: x
+        case _:
+            v = int(value)
+            value_lambda = lambda x: v
     test = int(test_text.lstrip().split(' ')[3])
     true = int(true_text.lstrip().split(' ')[5])
     false = int(false_text.lstrip().split(' ')[5])
-    return [items, operation, test, true, false, 0]
-
-def apply(operation, item):
-    op, arg = operation
-    v = item if arg == 'old' else int(arg)
-    return item * v if op == '*' else item + v
+    return [items, operation_lambda, value_lambda, test, true, false, 0]
 
 def do_round(monkeys, worry_reduce, modulo):
     for m in monkeys:
-        for item in m[0]:
-            new_worry = (apply(m[1], item) // worry_reduce) % modulo
-            target = m[3] if new_worry % m[2] == 0 else m[4]
-            monkeys[target][0].append(new_worry)
-            m[5] += 1
+        items, operation_lambda, value_lambda, test, true_target, false_target, _ = m
+        for item in items:
+            new_worry = (operation_lambda(item, value_lambda(item)) // worry_reduce) % modulo
+            monkeys[false_target if new_worry % test else true_target][0].append(new_worry)
+        m[6] += len(items)
         m[0] = []
 
 with open(sys.argv[1]) as f:
@@ -33,7 +33,7 @@ with open(sys.argv[1]) as f:
 
     modulo = functools.reduce(
         lambda a, b: a * b,
-        [m[2] for m in monkeys]
+        [m[3] for m in monkeys]
     )
 
     worry_reduce = int(sys.argv[3])
@@ -41,5 +41,5 @@ with open(sys.argv[1]) as f:
     for _ in range(int(sys.argv[2])):
         do_round(monkeys, worry_reduce, modulo)
 
-    throws = sorted([m[5] for m in monkeys])
+    throws = sorted([m[6] for m in monkeys])
     print(throws[-2] * throws[-1])
