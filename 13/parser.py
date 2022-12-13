c6888c101349033142527e608d1c7b0e7b3e958a
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-13 10:39:24 +0000

    Tidied up parser.

--- 13/parser.py
+++ 13/parser.py
@@ -2,30 +2,26 @@
 
 import sys
 
-def parse_(s, idx, target):
-    int_value = None
-    while True:
-        match s[idx]:
-            case '[':
-                l, idx = parse_(s, idx + 1, [])
-                target.append(l)
-            case c if c >= '0' and c <= '9':
-                i = ord(c) - 48
-                int_value = i if int_value is None else 10 * int_value + i
-            case ',':
-                if int_value is not None:
-                    target.append(int_value)
-                int_value = None
-            case ']':
-                if int_value is not None:
-                    target.append(int_value)
-                return target, idx
-            case _:
-                raise Exception(f'Unexpected character {s[idx]}')
-        idx += 1
+def append_maybe_int(l, maybe_int):
+    return l if maybe_int is None else l + [maybe_int]
+
+def parse_(s, l, maybe_int):
+    match s[0]:
+        case '[':
+            sublist, to_parse = parse_(s[1:], [], None)
+            return parse_(to_parse[1:], l + [sublist], None)
+        case c if c >= '0' and c <= '9':
+            i = ord(c) - 48
+            return parse_(s[1:], l, i if maybe_int is None else 10 * maybe_int + i)
+        case ',':
+            return parse_(s[1:], append_maybe_int(l, maybe_int), None)
+        case ']':
+            return append_maybe_int(l, maybe_int), s
+        case _:
+            raise Exception(f'Unexpected character {s[0]}')
 
 def parse(s):
-    parsed, _ = parse_(s, 1, [])
+    parsed, _ = parse_(s[1:], [], None)
     return parsed
 
 with open(sys.argv[1]) as f:
