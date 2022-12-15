11af9abbc8925b270925ac7ee30a881d86a545e7
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-15 12:16:58 +0000

    Further tidy, no more efficient.

--- 15/15.py
+++ 15/15.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 
+import functools
 import sys
 
-beacons_on_target_row = set()
+def reduce_ranges(a, b):
+    old_min_x, old_max_x, maybe_gap = a
+    new_min_x, new_max_x = b
+
+    if new_min_x > old_max_x + 1:
+        if maybe_gap:
+            raise Exception('Found multiple gaps')
+        maybe_gap = old_max_x + 1
+
+    return min(old_min_x, new_min_x), max(old_max_x, new_max_x), maybe_gap
 
 target_row = int(sys.argv[2])
 
-data = []
+sensors = []
+beacons_on_target_row = set()
 with open(sys.argv[1]) as f:
-    for l in [x.rstrip() for x in f]:
-        f = l.split()
-        sx = int(f[2][2:-1])
-        sy = int(f[3][2:-1])
-        nx = int(f[8][2:-1])
-        ny = int(f[9][2:])
+    for line in [x.rstrip() for x in f]:
+        fields = line.split()
+        sx = int(fields[2][2:-1])
+        sy = int(fields[3][2:-1])
+        nx = int(fields[8][2:-1])
+        ny = int(fields[9][2:])
         dist = abs(nx-sx) + abs(ny-sy)
-        data.append(((sx, sy), dist))
+        sensors.append((sx, sy, dist))
 
         if ny == target_row:
             beacons_on_target_row.add(nx)
@@ -24,22 +35,22 @@ part1 = None
 part2 = None
 
 for row in range(0, 4000000):
-    empty_ranges = []
-    for (sx, sy), dist in data:
-        row_delta = dist - abs(row-sy)
-        if row_delta < 0:
-            continue
-        empty_ranges.append((sx-row_delta, sx+row_delta))
+    empty_ranges = [
+        (sx-row_delta, sx+row_delta)
+        for sx, row_delta
+        in [
+            (sx, dist - abs(row-sy))
+            for sx, sy, dist
+            in sensors
+        ]
+        if row_delta >= 0
+    ]
 
-    empty_ranges = sorted(empty_ranges)
-    min_x, max_x = empty_ranges[0]
-    for new_min_x, new_max_x in empty_ranges[1:]:
-        if new_min_x > max_x + 1:
-            part2 = (max_x + 1) * 4000000 + row
-            break
+    sorted_ranges = sorted(empty_ranges)
+    min_x, max_x, gap = functools.reduce(reduce_ranges, sorted_ranges[1:], (*sorted_ranges[0], None))
 
-        min_x = min(min_x, new_min_x)
-        max_x = max(max_x, new_max_x)
+    if gap:
+        part2 = gap * 4000000 + row
 
     if row == target_row:
         part1 = max_x - min_x + 1 - len(beacons_on_target_row)
