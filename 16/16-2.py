e0c0e8ec642e3626f23f754ccb30fae122a56b18
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-16 16:14:38 +0000

    Works but *much* to slow on Part 2 real input.

--- 16/16-2.py
+++ 16/16-2.py
@@ -1,102 +1,110 @@
 #!/usr/bin/env python3
 
+import collections
 import sys
 
 MAX_TURNS = 26 + 1
 
+cave_dict = {}
 with open(sys.argv[1]) as f:
-    cave = {}
     for l in [x.rstrip() for x in f]:
         fields = l.split()
         flow_rate = int(fields[4][5:-1])
         tunnels = [x.replace(',', '') for x in fields[9:]]
-        cave[fields[1]] = (flow_rate, tunnels)
+        cave_dict[fields[1]] = (flow_rate, tunnels)
 
-def merge_valves(old_valves, new_valve):
-    x = set(old_valves)
-    x.add(new_valve)
-    return tuple(sorted(x))
-
-def merge_valves2(old_valves, new_valve1, new_valve2):
-    x = set(old_valves)
-    x.add(new_valve1)
-    x.add(new_valve2)
-    return tuple(sorted(x))
+indexes = {k: i for i, k in enumerate(cave_dict)}
+cave = [
+    (
+        flow_rate,
+        [indexes[t] for t in tunnels]
+    )
+    for k, (flow_rate, tunnels)
+    in cave_dict.items()
+]
 
 def move_and_move(state, location1, location2):
-    _, old_open, old_flow_rate, old_turns, old_pressure = state
+    _, valves, flow_rate, turns, pressure = state
     new_locations = (location1, location2) if location1 < location2 else (location2, location1)
-    return (new_locations, old_open, old_flow_rate, old_turns + 1, old_pressure + old_flow_rate)
+    return (new_locations, valves, flow_rate, turns + 1, pressure + flow_rate)
 
 def move_and_open(state, location1, valve):
-    (_, location2), old_open, old_flow_rate, old_turns, old_pressure = state
+    (_, location2), valves, flow_rate, turns, pressure = state
     new_locations = (location1, location2) if location1 < location2 else (location2, location1)
-    return (new_locations, merge_valves(old_open, valve), old_flow_rate + cave[valve][0], old_turns + 1, old_pressure + old_flow_rate)
+    return (new_locations, valves | 2**valve, flow_rate + cave[valve][0], turns + 1, pressure + flow_rate)
 
 def open_and_move(state, valve, location2):
-    (location1, _), old_open, old_flow_rate, old_turns, old_pressure = state
+    (location1, _), valves, flow_rate, turns, pressure = state
     new_locations = (location1, location2) if location1 < location2 else (location2, location1)
-    return (new_locations, merge_valves(old_open, valve), old_flow_rate + cave[valve][0], old_turns + 1, old_pressure + old_flow_rate)
+    return (new_locations, valves | 2**valve, flow_rate + cave[valve][0], turns + 1, pressure + flow_rate)
 
 def open_and_open(state, valve1, valve2):
-    locations, old_open, old_flow_rate, old_turns, old_pressure = state
-    new_open = merge_valves2(old_open, valve1, valve2)
-    return (locations, new_open, old_flow_rate + cave[valve1][0] + cave[valve2][0], old_turns + 1, old_pressure + old_flow_rate)
+    locations, valves, flow_rate, turns, pressure = state
+    return (locations, valves | 2**valve1 | 2**valve2, flow_rate + cave[valve1][0] + cave[valve2][0], turns + 1, pressure + flow_rate)
 
 def moves(state):
-    (old_location1, old_location2), old_open, old_flow_rate, turns, pressure = state
-
-    if turns == MAX_TURNS:
-        return
+    (old_location1, old_location2), valves, flow_rate, turns, pressure = state
 
     for new_location1 in cave[old_location1][1]:
         for new_location2 in cave[old_location2][1]:
             yield move_and_move(state, new_location1, new_location2)
 
-        if old_location2 not in old_open and cave[old_location2][0] > 0:
+        if not (valves & (2**old_location2)) and cave[old_location2][0] > 0:
             yield move_and_open(state, new_location1, old_location2)
 
-    if old_location1 not in old_open and cave[old_location1][0] > 0:
+    if not (valves & (2**old_location1)) and cave[old_location1][0] > 0:
         for new_location2 in cave[old_location2][1]:
             yield open_and_move(state, old_location1, new_location2)
 
-        if old_location1 != old_location2 and old_location2 not in old_open and cave[old_location2][0] > 0:
+        if old_location1 != old_location2 and not (valves & (2**old_location2)) and cave[old_location2][0] > 0:
             yield open_and_open(state, old_location1, old_location2)
 
-def state_to_seen(state):
-    locations, open_valves, flow_rate, turns, pressure = state
-    return (locations, open_valves)
-
-initial_state = (('AA', 'AA'), tuple(), 0, 1, 0)
+initial_state = ((indexes['AA'], indexes['AA']), 0, 0, 1, 0)
 todo = [initial_state]
-seen = {state_to_seen(initial_state): initial_state[4]}
-
-all_pressure = sum([fr for (fr, _) in cave.values()])
-
-n = 0
+seen = collections.defaultdict(dict)
+seen[initial_state[0]][initial_state[1]] = initial_state[4]
 
 best_so_far = 0
 
+def prune(state, seen):
+    for valves, pressure in seen.items():
+        if (state[1] & ~valves) == 0 and pressure > state[4]:
+            return True
+
+    return False
+
+def should(state, seen):
+    result = True
+    to_remove = []
+
+    for valves, pressure in seen.items():
+
+        if (state[1] & ~valves) == 0 and pressure >= state[4]:
+            result = False
+            break
+
+        if (valves & ~state[1]) == 0 and state[4] > pressure:
+            to_remove.append(valves)
+
+    for k in to_remove:
+        del seen[k]
+
+    if result:
+        seen[state[1]] = state[4]
+
+    return result
+
 while todo:
     state = todo.pop(0)
-    n += 1
 
-    key = state_to_seen(state)
-    if seen[key] > state[4]:
-        continue
-
-    best_possible = state[4] + (MAX_TURNS - state[3]) * all_pressure
-    if best_possible < best_so_far:
+    if prune(state, seen[state[0]]):
         continue
 
     for ns in moves(state):
-        ns_key = state_to_seen(ns)
-        if ns_key not in seen or ns[4] > seen[ns_key]:
-            todo.append(ns)
-            seen[ns_key] = ns[4]
+        if should(ns, seen[ns[0]]):
+            if ns[3] < MAX_TURNS:
+                todo.append(ns)
             if ns[4] > best_so_far:
                 best_so_far = ns[4]
-                print(best_so_far, ns)
 
-print(max(seen.values()))
-print(n)
+print(best_so_far)
--- 16/16.py
+++ 16/16.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 
+import collections
 import sys
 
 MAX_TURNS = 30 + 1
 
+cave_dict = {}
 with open(sys.argv[1]) as f:
-    cave = {}
     for l in [x.rstrip() for x in f]:
         fields = l.split()
         flow_rate = int(fields[4][5:-1])
         tunnels = [x.replace(',', '') for x in fields[9:]]
-        cave[fields[1]] = (flow_rate, tunnels)
+        cave_dict[fields[1]] = (flow_rate, tunnels)
+
+indexes = {k: i for i, k in enumerate(cave_dict)}
+cave = [
+    (
+        flow_rate,
+        [indexes[t] for t in tunnels]
+    )
+    for k, (flow_rate, tunnels)
+    in cave_dict.items()
+]
 
 def merge_valves(old_valves, new_valve):
-    x = set(old_valves)
-    x.add(new_valve)
-    return tuple(sorted(x))
+    return old_valves | (2**new_valve)
 
 def open_valve(state, valve):
     old_location, old_open, old_flow_rate, old_turns, old_pressure = state
@@ -34,44 +43,55 @@ def moves(state):
     for new_location in cave[old_location][1]:
         yield move(state, new_location)
 
-    if old_location not in old_open and cave[old_location][0] > 0:
+    if not (old_open & (2**old_location)) and cave[old_location][0] > 0:
         yield open_valve(state, old_location)
 
-def state_to_seen(state):
-    location, open_valves, flow_rate, turns, pressure = state
-    return (location, open_valves)
-
-initial_state = ('AA', tuple(), 0, 1, 0)
+initial_state = (indexes['AA'], 0, 0, 1, 0)
 todo = [initial_state]
-seen = {state_to_seen(initial_state): initial_state[4]}
-
-all_pressure = sum([fr for (fr, _) in cave.values()])
-print(all_pressure)
-
-n = 0
+seen = [{} for _ in cave]
+seen[initial_state[0]][initial_state[1]] = initial_state[4]
 
 best_so_far = 0
 
+def should(state, seen, foo):
+    result = True
+    to_remove = []
+
+    for valves, pressure in seen[state[0]].items():
+
+        extra_valves_new = state[1] & ~valves
+        if extra_valves_new == 0 and pressure >= state[4] + foo:
+            result = False
+            break
+
+        extra_valves_old = valves & ~state[1]
+        if extra_valves_old == 0 and state[4] > pressure:
+            to_remove.append(valves)
+
+    if result:
+        for k in to_remove:
+            del seen[state[0]][k]
+
+    return result
+
+def dump_seen(s):
+    for k, v in s.items():
+        print(k)
+        for k2, v2 in v.items():
+            print(f'{k2:x}, {v2}')
+        print()
+
 while todo:
     state = todo.pop(0)
-    n += 1
 
-    key = state_to_seen(state)
-    if seen[key] > state[4]:
-        continue
-
-    best_possible = state[4] + (MAX_TURNS - state[3]) * all_pressure
-    if best_possible < best_so_far:
+    if not should(state, seen, 1):
         continue
 
     for ns in moves(state):
-        ns_key = state_to_seen(ns)
-        if ns_key not in seen or ns[4] > seen[ns_key]:
+        if should(ns, seen, 0):
             todo.append(ns)
-            seen[ns_key] = ns[4]
+            seen[ns[0]][ns[1]] = ns[4]
             if ns[4] > best_so_far:
                 best_so_far = ns[4]
-                print(best_so_far, ns)
 
-print(max(seen.values()))
-print(n)
+print(best_so_far)
