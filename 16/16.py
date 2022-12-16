f6e31f46158ff22e155ddc9b57b9c56d8370e2e3
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-16 16:34:22 +0000

    Merge both parts.

--- 16/16.py
+++ 16/16.py
@@ -3,8 +3,6 @@
 import collections
 import sys
 
-MAX_TURNS = 30 + 1
-
 cave_dict = {}
 with open(sys.argv[1]) as f:
     for l in [x.rstrip() for x in f]:
@@ -31,7 +29,7 @@ def move(state, location):
     _, valves, flow_rate, turns, pressure = state
     return (location, valves, flow_rate, turns + 1, pressure + flow_rate)
 
-def moves(state):
+def moves_part1(state):
     old_location, old_open, old_flow_rate, turns, pressure = state
 
     for new_location in cave[old_location][1]:
@@ -40,9 +38,53 @@ def moves(state):
     if not (old_open & (2**old_location)) and cave[old_location][0] > 0:
         yield open_valve(state, old_location)
 
-initial_state = (indexes['AA'], 0, 0, 1, 0)
+def move_and_move(state, location1, location2):
+    _, valves, flow_rate, turns, pressure = state
+    new_locations = (location1, location2) if location1 < location2 else (location2, location1)
+    return (new_locations, valves, flow_rate, turns + 1, pressure + flow_rate)
+
+def move_and_open(state, location1, valve):
+    (_, location2), valves, flow_rate, turns, pressure = state
+    new_locations = (location1, location2) if location1 < location2 else (location2, location1)
+    return (new_locations, valves | 2**valve, flow_rate + cave[valve][0], turns + 1, pressure + flow_rate)
+
+def open_and_move(state, valve, location2):
+    (location1, _), valves, flow_rate, turns, pressure = state
+    new_locations = (location1, location2) if location1 < location2 else (location2, location1)
+    return (new_locations, valves | 2**valve, flow_rate + cave[valve][0], turns + 1, pressure + flow_rate)
+
+def open_and_open(state, valve1, valve2):
+    locations, valves, flow_rate, turns, pressure = state
+    return (locations, valves | 2**valve1 | 2**valve2, flow_rate + cave[valve1][0] + cave[valve2][0], turns + 1, pressure + flow_rate)
+
+def moves_part2(state):
+    (old_location1, old_location2), valves, flow_rate, turns, pressure = state
+
+    for new_location1 in cave[old_location1][1]:
+        for new_location2 in cave[old_location2][1]:
+            yield move_and_move(state, new_location1, new_location2)
+
+        if not (valves & (2**old_location2)) and cave[old_location2][0] > 0:
+            yield move_and_open(state, new_location1, old_location2)
+
+    if not (valves & (2**old_location1)) and cave[old_location1][0] > 0:
+        for new_location2 in cave[old_location2][1]:
+            yield open_and_move(state, old_location1, new_location2)
+
+        if old_location1 != old_location2 and not (valves & (2**old_location2)) and cave[old_location2][0] > 0:
+            yield open_and_open(state, old_location1, old_location2)
+
+if int(sys.argv[2]):
+    initial_state = ((indexes['AA'], indexes['AA']), 0, 0, 1, 0)
+    moves = moves_part2
+    max_turns = 26 + 1
+else:
+    initial_state = (indexes['AA'], 0, 0, 1, 0)
+    moves = moves_part1
+    max_turns = 30 + 1
+
 todo = [initial_state]
-seen = [{} for _ in cave]
+seen = collections.defaultdict(dict)
 seen[initial_state[0]][initial_state[1]] = initial_state[4]
 
 best_so_far = 0
@@ -85,7 +127,7 @@ while todo:
 
     for ns in moves(state):
         if should(ns, seen[ns[0]]):
-            if ns[3] < MAX_TURNS:
+            if ns[3] < max_turns:
                 todo.append(ns)
             if ns[4] > best_so_far:
                 best_so_far = ns[4]
