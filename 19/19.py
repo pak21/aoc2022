3a4fd071343e7d90b90071acc0ff39ebdc44e2d8
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-19 13:49:19 +0000

    Slightly more structured objects.

--- 19/19.py
+++ 19/19.py
@@ -18,76 +18,56 @@ with open(sys.argv[1]) as f:
 
 def build_ore(state, bp):
     return (
-        state[0] + 1,
-        state[1] - bp[0],
+        (state[0][0] + 1, state[0][1] - bp[0]),
+        state[1],
         state[2],
         state[3],
-        state[4],
-        state[5],
-        state[6],
-        state[7],
-        state[8]
+        state[4]
     )
 
 def build_clay(state, bp):
     return (
-        state[0],
-        state[1] - bp[1],
-        state[2] + 1,
+        (state[0][0], state[0][1] - bp[1]),
+        (state[1][0] + 1, state[1][1]),
+        state[2],
         state[3],
-        state[4],
-        state[5],
-        state[6],
-        state[7],
-        state[8]
+        state[4]
     )
 
 def build_obsidian(state, bp):
     return (
-        state[0],
-        state[1] - bp[2][0],
-        state[2],
-        state[3] - bp[2][1],
-        state[4] + 1,
-        state[5],
-        state[6],
-        state[7],
-        state[8]
+        (state[0][0], state[0][1] - bp[2][0]),
+        (state[1][0], state[1][1] - bp[2][1]),
+        (state[2][0] + 1, state[2][1]),
+        state[3],
+        state[4],
     )
 
 def build_geode(state, bp):
     return (
-        state[0],
-        state[1] - bp[3][0],
-        state[2],
-        state[3],
-        state[4],
-        state[5] - bp[3][1],
-        state[6] + 1,
-        state[7],
-        state[8]
+        (state[0][0], state[0][1] - bp[3][0]),
+        state[1],
+        (state[2][0], state[2][1] - bp[3][1]),
+        (state[3][0] + 1, state[3][1]),
+        state[4]
     )
 
 def collect(state):
     return (
-        state[0],
-        state[1] + state[0],
-        state[2],
-        state[3] + state[2],
-        state[4],
-        state[5] + state[4],
-        state[6],
-        state[7] + state[6],
-        state[8] + 1
+        (state[0][0], state[0][1] + state[0][0]),
+        (state[1][0], state[1][1] + state[1][0]),
+        (state[2][0], state[2][1] + state[2][0]),
+        (state[3][0], state[3][1] + state[3][0]),
+        state[4] + 1
     )
 
 def run_minute(state, bp, max_geodes):
-    minutes_to_go = max_minutes - state[8]
+    minutes_to_go = max_minutes - state[4]
 
     if minutes_to_go == 0:
         return
 
-    max_collectible = state[7] + (state[6] * minutes_to_go) + (minutes_to_go * (minutes_to_go - 1) // 2)
+    max_collectible = state[3][1] + (state[3][0] * minutes_to_go) + (minutes_to_go * (minutes_to_go - 1) // 2)
 
     if max_collectible <= max_geodes:
         return
@@ -96,20 +76,20 @@ def run_minute(state, bp, max_geodes):
 
     yield collected
 
-    if state[1] >= bp[0]:
+    if state[0][1] >= bp[0]:
         yield build_ore(collected, bp)
 
-    if state[1] >= bp[1]:
+    if state[0][1] >= bp[1]:
         yield build_clay(collected, bp)
 
-    if state[1] >= bp[2][0] and state[3] >= bp[2][1]:
+    if state[0][1] >= bp[2][0] and state[1][1] >= bp[2][1]:
         yield build_obsidian(collected, bp)
 
-    if state[1] >= bp[3][0] and state[5] >= bp[3][1]:
+    if state[0][1] >= bp[3][0] and state[2][1] >= bp[3][1]:
         yield build_geode(collected, bp)
 
 def run_bp(bp):
-    initial_state = (1, 0, 0, 0, 0, 0, 0, 0, 0)
+    initial_state = ((1, 0), (0, 0), (0, 0), (0, 0), 0)
 
     seen = {initial_state}
     todo = [initial_state]
@@ -123,8 +103,8 @@ def run_bp(bp):
                 seen.add(ns)
                 todo.append(ns)
 
-                if ns[7] > max_geodes:
-                    max_geodes = ns[7]
+                if ns[3][1] > max_geodes:
+                    max_geodes = ns[3][1]
 
     return max_geodes, len(seen)
 
