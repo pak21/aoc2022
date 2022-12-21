cf08775fc938e63c98a1cc2d088ea398a5457f67
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-21 08:28:04 +0000

    Remove unused cache.

--- 21/21.py
+++ 21/21.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python3
 
-import collections
 import sys
-
-import numpy as np
 
 monkeys = {}
 with open(sys.argv[1]) as f:
-    for l in [x.rstrip() for x in f]:
-        args = l.split()
+    for args in [x.rstrip().split() for x in f]:
         monkeys[args[0][:-1]] = args[1:]
 
-values = {}
-
-def eval_monkey(monkey, monkeys, values):
+def eval_monkey(monkey, monkeys):
     args = monkeys[monkey]
     if len(args) == 1:
-        value = int(args[0])
+        return int(args[0])
     else:
-        value1 = eval_monkey(args[0], monkeys, values)
-        value2 = eval_monkey(args[2], monkeys, values)
+        value1 = eval_monkey(args[0], monkeys)
+        value2 = eval_monkey(args[2], monkeys)
         match args[1]:
             case '+':
-                value = value1 + value2
+                return value1 + value2
             case '-':
-                value = value1 - value2
+                return value1 - value2
             case '*':
-                value = value1 * value2
+                return value1 * value2
             case '/':
-                value = value1 / value2
+                return value1 // value2
             case _:
                 raise Exception(args[1])
 
-    values[monkey] = value
-    return value
-
-print(int(eval_monkey('root', monkeys, values)))
+print(eval_monkey('root', monkeys))
