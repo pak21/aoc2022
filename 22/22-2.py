e254b2e2e1b5c703baf674213eeee77feca51bc8
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-22 09:35:34 +0000

    Some tidying.

--- 22/22-2.py
+++ 22/22-2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
-import collections
 import sys
-
-import numpy as np
 
 DIRS = [
     (0, 1),
@@ -37,15 +34,21 @@ direction = 0
 
 def wrap(grid, y, x, direction, test_geometry):
     if test_geometry:
-        match (y, x, direction):
-            case (5, 11, 0):
-                return [8, 14], 1
-            case (11, 10, 1):
-                return [7, 1], 3
-            case (4, 6, 3):
-                return [2, 8], 0
-            case _:
-                raise Exception(f'test {loc} {direction}')
+        match direction:
+            case 0:
+                if x == 11 and y >= 4 and y <= 7:
+                    return [8, 19-y], 1
+
+            case 1:
+                if y == 11 and x >= 8 and x <= 11:
+                    return [7, 11-x], 3
+
+            case 2:
+                pass
+
+            case 3:
+                if y == 4 and x >= 4 and x <= 7:
+                    return [x-4, 8], 0
     else:
         match direction:
             case 0:
@@ -61,7 +64,6 @@ def wrap(grid, y, x, direction, test_geometry):
                 if x == 149 and y >= 0 and y <= 49:
                     return [149-y, 99], 2
 
-                raise Exception(f'input {loc} {direction}')
             case 1:
                 if y == 49 and x >= 100 and x <= 149:
                     return [x-50, 99], 2
@@ -72,7 +74,6 @@ def wrap(grid, y, x, direction, test_geometry):
                 if y == 199 and x >= 0 and x <= 49:
                     return [0, 100+x], 1 
 
-                raise Exception(f'input {loc} {direction}')
             case 2:
                 if x == 0 and y >= 100 and y <= 149:
                     return [149-y, 50], 0
@@ -86,7 +87,6 @@ def wrap(grid, y, x, direction, test_geometry):
                 if x == 50 and y >= 50 and y <= 99:
                     return [100, y-50], 1
 
-                raise Exception(f'input {loc} {direction}')
             case 3:
                 if y == 0 and x >= 50 and x <= 99:
                     return [x+100, 0], 0
@@ -97,16 +97,7 @@ def wrap(grid, y, x, direction, test_geometry):
                 if y == 100 and x >= 0 and x <= 49:
                     return [x+50, 50], 0
                     
-                raise Exception(f'input {loc} {direction}')
-
-def get_next_tile(grid, y, x):
-    if y < 0 or y >= len(grid):
-        return ' '
-
-    if x < 0 or x >= len(grid[y]):
-        return ' '
-
-    return grid[y][x]
+        raise Exception(f'{test_geometry} {loc} {direction}')
 
 for move in moves:
     match move:
@@ -117,25 +108,21 @@ for move in moves:
         case _:
             for _ in range(move):
                 step = DIRS[direction]
-                next_loc = [loc[0] + step[0], loc[1] + step[1]]
-                next_tile = get_next_tile(grid, next_loc[0], next_loc[1])
+                y, x = loc[0] + step[0], loc[1] + step[1]
+                next_tile = grid[y][x] if y >= 0 and y < len(grid) and x >= 0 and x < len(grid[y]) else ' '
                 next_direction = direction
 
                 if next_tile == ' ':
-                    next_loc, next_direction = wrap(grid, loc[0], loc[1], direction, test_geometry)
-                    next_tile = grid[next_loc[0]][next_loc[1]]
+                    (y, x), next_direction = wrap(grid, loc[0], loc[1], direction, test_geometry)
+                    next_tile = grid[y][x]
 
                 match next_tile:
                     case '.':
-                        loc = next_loc
+                        loc = (y, x)
                         direction = next_direction
                     case '#':
                         break
                     case _:
                         raise Exception(f'Invalid tile: "{next_tile}"')
 
-row = loc[0] + 1
-column = loc[1] + 1
-
-password = 1000 * row + 4 * column + direction
-print(password)
+print(1000 * (loc[0] + 1) + 4 * (loc[1] + 1) + direction)
--- 22/22.py
+++ 22/22.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
-import collections
 import sys
-
-import numpy as np
 
 DIRS = [
     (0, 1),
@@ -28,43 +25,20 @@ for c in moves_text:
         n = 0
 moves.append(n)
 
-start_x = grid[0].index('.')
-
-loc = (0, start_x)
+loc = (0, grid[0].index('.'))
 direction = 0
 
-def wrap_right(grid, y):
-    x = 0
+def wrap_horizontal(grid, y, x0, dx):
+    x = x0
     while grid[y][x] == ' ':
-        x += 1
+        x += dx
     return x
 
-def wrap_left(grid, y):
-    x = len(grid[y]) - 1
-    while grid[y][x] == ' ':
-        x -= 1
-    return x
-
-def wrap_down(grid, x):
-    y = 0
-    while grid[y][x] == ' ':
-        y += 1
-    return y
-
-def wrap_up(grid, x):
-    y = len(grid) - 1
+def wrap_vertical(grid, x, y0, dy):
+    y = y0
     while x >= len(grid[y]) or grid[y][x] == ' ':
-        y -= 1
+        y += dy
     return y
-
-def get_next_tile(grid, y, x):
-    if y < 0 or y >= len(grid):
-        return ' '
-
-    if x < 0 or x >= len(grid[y]):
-        return ' '
-
-    return grid[y][x]
 
 for move in moves:
     match move:
@@ -75,34 +49,30 @@ for move in moves:
         case _:
             for _ in range(move):
                 step = DIRS[direction]
-                next_loc = [loc[0] + step[0], loc[1] + step[1]]
-                next_tile = get_next_tile(grid, next_loc[0], next_loc[1])
+                y, x = loc[0] + step[0], loc[1] + step[1]
+                next_tile = grid[y][x] if y >= 0 and y < len(grid) and x >= 0 and x < len(grid[y]) else ' '
 
                 if next_tile == ' ':
                     match direction:
                         case 0:
-                            next_loc[1] = wrap_right(grid, next_loc[0])
+                            x = wrap_horizontal(grid, y, 0, 1)
                         case 1:
-                            next_loc[0] = wrap_down(grid, next_loc[1])
+                            y = wrap_vertical(grid, x, 0, 1)
                         case 2:
-                            next_loc[1] = wrap_left(grid, next_loc[0])
+                            x = wrap_horizontal(grid, y, len(grid[y]) - 1, -1)
                         case 3:
-                            next_loc[0] = wrap_up(grid, next_loc[1])
+                            y = wrap_vertical(grid, x, len(grid) - 1, -1)
                         case _:
-                            raise Exception('nope')
+                            raise Exception(f'Invalid direction: {direction}')
 
-                    next_tile = grid[next_loc[0]][next_loc[1]]
+                    next_tile = grid[y][x]
 
                 match next_tile:
                     case '.':
-                        loc = next_loc
+                        loc = (y, x)
                     case '#':
                         break
                     case _:
                         raise Exception(f'Invalid tile: "{next_tile}"')
 
-row = loc[0] + 1
-column = loc[1] + 1
-
-password = 1000 * row + 4 * column + direction
-print(password)
+print(1000 * (loc[0] + 1) + 4 * (loc[1] + 1) + direction)
