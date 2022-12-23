8dc82d7e338cf6f079267e07bee3edf2d93222e4
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-23 09:04:51 +0000

    Tidy.

--- 23/23.py
+++ 23/23.py
@@ -3,60 +3,32 @@
 import collections
 import sys
 
-import numpy as np
+def prop_ns(y, x, elves):
+    return (y, x) if (y, x-1) not in elves and (y, x) not in elves and (y, x+1) not in elves else None
 
-with open(sys.argv[1]) as f:
-    grid = np.array([[c == '#' for c in l.strip()] for l in f])
+def prop_ew(y, x, elves):
+    return (y, x) if (y-1, x) not in elves and (y, x) not in elves and (y+1, x) not in elves else None
 
-elves = {k for k, v in np.ndenumerate(grid) if v}
+LOOKS = [(y, x) for y in range(-1, 2) for x in range(-1, 2) if (y, x) != (0, 0)]
 
-def prop_n(y, x, elves, proposed):
-    if (y-1, x-1) not in elves and (y-1, x) not in elves and (y-1, x+1) not in elves:
-        proposed[(y-1, x)].append((y, x))
-        return True
-    return False
-    
-def prop_s(y, x, elves, proposed):
-    if (y+1, x-1) not in elves and (y+1, x) not in elves and (y+1, x+1) not in elves:
-        proposed[(y+1, x)].append((y, x))
-        return True
-    return False
-
-def prop_w(y, x, elves, proposed):
-    if (y-1, x-1) not in elves and (y, x-1) not in elves and (y+1, x-1) not in elves:
-        proposed[(y, x-1)].append((y, x))
-        return True
-    return False
-
-def prop_e(y, x, elves, proposed):
-    if (y-1, x+1) not in elves and (y, x+1) not in elves and (y+1, x+1) not in elves:
-        proposed[(y, x+1)].append((y, x))
-        return True
-    return False
-
-PROPS = [prop_n, prop_s, prop_w, prop_e]
+PROPS = [
+    lambda y, x, e: prop_ns(y-1, x, e),
+    lambda y, x, e: prop_ns(y+1, x, e),
+    lambda y, x, e: prop_ew(y, x-1, e),
+    lambda y, x, e: prop_ew(y, x+1, e)
+]
 
 def do_round(elves, n):
     somebody_moved = False
     proposed = collections.defaultdict(list)
 
     for y, x in elves:
-
-        found = False
-        for dy in [-1, 0, 1]:
-            for dx in [-1, 0, 1]:
-                if dy == 0 and dx == 0:
-                    continue
-
-                if (y+dy, x+dx) in elves:
-                    found = True
+        if any([(y+dy, x+dx) in elves for dy, dx in LOOKS]):
+            for i in range(4):
+                move = PROPS[(n+i) % 4](y, x, elves)
+                if move:
+                    proposed[move].append((y, x))
                     break
-
-        if found:
-            if not PROPS[(n+0) % 4](y, x, elves, proposed):
-                if not PROPS[(n+1) % 4](y, x, elves, proposed):
-                    if not PROPS[(n+2) % 4](y, x, elves, proposed):
-                        PROPS[(n+3) % 4](y, x, elves, proposed)
                 
     for (ty, tx), movers in proposed.items():
         if len(movers) == 1:
@@ -67,14 +39,15 @@ def do_round(elves, n):
     return somebody_moved
 
 def score(elves):
-    s = 0
     min_y = min([y for y, _ in elves])
     max_y = max([y for y, _ in elves])
     min_x = min([x for _, x in elves])
     max_x = max([x for _, x in elves])
 
-    s = (max_y - min_y + 1) * (max_x - min_x + 1) - len(elves)
-    return s
+    return (max_y - min_y + 1) * (max_x - min_x + 1) - len(elves)
+
+with open(sys.argv[1]) as f:
+    elves = {(y, x) for y, l in enumerate(f) for x, c in enumerate(l) if c == '#'}
 
 r = 0
 while do_round(elves, r):
