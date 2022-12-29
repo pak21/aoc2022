91c9ff8ad3309b4877eb68fdcd0a03ae3dc3561b
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-29 12:35:10 +0000

    Merge both parts.

--- 05/05.py
+++ 05/05.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 
-import collections
 import sys
 
+append_step = -1 if sys.argv[2] == '1' else 1
+
 with open(sys.argv[1]) as f:
-    t = f.read().rstrip()
-    start_t, moves_t = t.split('\n\n')
+    start_t, moves_t = f.read().rstrip().split('\n\n')
 
-    config = [[]] * 9
-    for l in start_t.split('\n'):
-        for i in range((len(l) + 1) // 4):
-            t = l[4*i:4*i+3]
-            if t[0] == '[':
-                config[i] = [t[1]] + config[i]
+config = [[]] * 9
+for l in start_t.split('\n'):
+    for i in range((len(l) + 1) // 4):
+        t = l[4*i:4*i+3]
+        if t[0] == '[':
+            config[i] = [t[1]] + config[i]
 
-    for l in moves_t.split('\n'):
-        _, n, _, src, _, dest = l.split()
-        n = int(n)
-        src = int(src) - 1
-        dest = int(dest) - 1
+for l in moves_t.split('\n'):
+    _, n, _, src, _, dest = l.split()
+    n = int(n)
+    src = int(src) - 1
+    dest = int(dest) - 1
 
-        for i in range(n):
-            config[dest].append(config[src][-1])
-            config[src] = config[src][:-1]
+    config[dest] = config[dest] + config[src][-n:][::append_step]
+    config[src] = config[src][:-n]
 
-    print(''.join([stack[-1] for stack in config]))
+print(''.join([stack[-1] for stack in config]))
