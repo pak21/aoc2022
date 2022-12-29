1c7dd692f3544e9b1a9600bb476136abde9a962a
Phil Kendall <philip@shadowmagic.org.uk>
2022-12-29 16:03:31 +0000

    Stop / explosion.
    Makes no difference to the result but is much cleaner and more logical.

--- 07/07.py
+++ 07/07.py
@@ -3,20 +3,20 @@
 import collections
 import sys
 
+sizes = collections.defaultdict(int)
+
 with open(sys.argv[1]) as f:
-    path = []
-    currentdir = ''
-    sizes = collections.defaultdict(int)
     for l in [x.rstrip() for x in f]:
         if l[0:2] == '$ ':
             if l[2:5] == 'cd ':
                 newdir = l[5:]
-                if newdir == '..':
-                    path = path[:-1]
-                    currentdir = '/'.join(path)
-                else:
-                    currentdir = currentdir + '/' + newdir
-                    path.append(currentdir)
+                match newdir:
+                    case '/':
+                        path = ['']
+                    case '..':
+                        path = path[:-1]
+                    case _:
+                        path.append(path[-1] + '/' + newdir)
         else:
             size, name = l.split()
             if size == 'dir':
@@ -25,7 +25,7 @@ with open(sys.argv[1]) as f:
                 for component in path:
                     sizes[component] += int(size)
 
-    print(sum([size for size in sizes.values() if size <= 100000]))
+print(sum([size for size in sizes.values() if size <= 100000]))
 
-    required = sizes['//'] - 40000000
-    print(sorted([size for size in sizes.values() if size >= required])[0])
+required = sizes[''] - 40000000
+print(sorted([size for size in sizes.values() if size >= required])[0])
